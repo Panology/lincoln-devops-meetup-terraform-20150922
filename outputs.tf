398a61275a1979d6c3c6bb470de6b61cea53ba04
Jason Wilkinson <jason.wilkinson@firespring.com>
2015-09-22 17:07:32 -0500

    Add two web servers and an ELB

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
+output "app.0.ip" {
+  value = "${aws_instance.app.0.private_ip}"
+}
+
+output "app.1.ip" {
+  value = "${aws_instance.app.1.private_ip}"
+}
+
+/*output "appservers" {
+    value = "${join(",", aws_instance.app.*.private_ip)}"
+}*/
+
 output "nat.ip" {
   value = "${aws_instance.nat.public_ip}"
+}
+
+output "elb.hostname" {
+  value = "${aws_elb.app.dns_name}"
 }
--- security-groups.tf
+++ security-groups.tf
@@ -55,3 +55,28 @@ resource "aws_security_group" "nat" {
     Name = "nat-meetup-example"
   }
 }
+
+/* Security group for the web */
+resource "aws_security_group" "web" {
+  name = "web-meetup-example"
+  description = "Security group for web that allows web traffic from internet"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "web-meetup-example"
+  }
+}
